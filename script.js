7dcbe16ffbf7ccd6c07b95db32354f4201e75fee
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-07-27 23:41:13 +0530

    new changes

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ const restaurants = [
     cuisine: "Italian",
     location: "Mumbai",
     rating: 4.0,
-    // image: "assets/images/Pizza_hut.svg",
-    image: "https://via.placeholder.com/300x180?text=Pizza+Hut"
+    image: "assets/images/Pizza_hut.svg",
 
 
 },
